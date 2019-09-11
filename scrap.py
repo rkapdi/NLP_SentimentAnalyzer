c8f816d8448e4e175f86fb51865c6313ff2d0dd9
rkapdi <riss.ak@hotmail.com>
2019-09-11 12:45:41 -0700

    Add files via upload

---
+++ scrap.py
@@ -0,0 +1,32 @@
+# Importing libraries and dependencies
+
+import tweepy
+import csv
+import pandas as pd
+
+# twitter API access credentials
+
+#Consumer API keys
+consumer_key = 'i8szMdYgwlqZrXmFJk2PGNBmd' #(API key)
+consumer_secret = 'srv0vtL93WVpsoQnYtj1Tr6UHqunu8ISIODVQVWYtvc42wqevj' #(API secret key)
+
+# Access token & access token secret
+access_token = '567062540-2EdLNiAtOKrIqTJsnkSYiTpvCihnIy5ZFCKkm45V' #(Access token)
+access_secret = 'JIvd0LRdIg7e1tx1ltvTmVUc1JChNLSJyiqA4VKdC8Ks9' #(Access token secret)
+# Read and write (Access level)
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+###UNITED AIRLINES###
+
+#Open/Create a file to append data
+csvFile = open('ua.csv','a')
+
+#use a csv writer
+csvWriter = csv.writer(csvFile)
+
+for tweet in tweepy.Cursor(api.search, q="#unitedAIRLINES", count=100, lang="en", since="2017-04-03").items():
+    print (tweet.created_at, tweet.text)
+    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
